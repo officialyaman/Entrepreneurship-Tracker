e399934bde0d46c5e6393b0b1b2d738fd6eace06
officialyaman <homsiyaman2014@gmail.com>
2026-03-06 15:27:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
+const supabase = supabase.createClient(
+"https://hrcaisojgvuulwubaqez.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+)
 
+async function login(){
+
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+const {data,error}=await supabase.auth.signInWithPassword({
+email:email,
+password:password
+})
+
+if(error){
+alert(error.message)
+return
+}
+
+document.getElementById("login").style.display="none"
+document.getElementById("dashboard").style.display="block"
+
+loadLogs()
+
+}
+
+async function addMoney(){
+
+const title=document.getElementById("title").value
+const amount=document.getElementById("amount").value
+const type=document.getElementById("type").value
+
+const user=await supabase.auth.getUser()
+
+await supabase
+.from("money_logs")
+.insert([
+{
+title:title,
+amount:amount,
+type:type,
+user_id:user.data.user.id
+}
+])
+
+loadLogs()
+
+}
+
+async function loadLogs(){
+
+const {data}=await supabase
+.from("money_logs")
+.select("*")
+.order("id",{ascending:false})
+
+let html=""
+
+data.forEach(log=>{
+html+=`
+<div class="log">
+${log.title} - $${log.amount} (${log.type})
+</div>
+`
+})
+
+document.getElementById("logs").innerHTML=html
+
+}
